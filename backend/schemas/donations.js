5a068626321c0739bd7683c37465aeba4a07fa25
Amna Shah <75795497+amnashah110@users.noreply.github.com>
2025-04-30 08:21:52 +0500

    Update donations.js

--- backend/schemas/donations.js
+++ backend/schemas/donations.js
@@ -4,7 +4,7 @@ const { Schema } = mongoose;
 const donationSchema = new Schema({
     username: {
         type: String,
-        required: true
+        required: true,
     },
     foodType: {
         type: String,
