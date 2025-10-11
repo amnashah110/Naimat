9de7da4109c004a273a74cee60f87d9231720c60
Amna Shah <75795497+amnashah110@users.noreply.github.com>
2025-10-11 11:12:12 +0500

    Delete ai-service directory

--- ai-service/app/predict_expiry.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter()
-
-@router.get("/predict-expiry")
-def predict_expiry():
-    return {"endpoint": "predict-expiry"}
--- ai-service/app/upload_image.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter()
-
-@router.get("/upload-image")
-def upload_image():
-    return {"endpoint": "upload-image"}
